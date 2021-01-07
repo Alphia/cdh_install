4f3dde5dc32b08d4b88b80f6be6c61302a7b3de6
root <root@host0.mycluster.com>
2021-01-07 19:20:42 +0800

    set host & domain

---
+++ set_host_domain.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+
+
+hostnamectl set-hostname $1.$2
+
+sed -i -e "s/127.0.0.1.*/127.0.0.1 $1 $1.$2/" /etc/hosts
+
+echo "" >> /etc/hosts
+cat host_list >> /etc/hosts
+
+sed -i -e "s/HOSTNAME.*/HOSTNAME=$1.$2/" /etc/sysconfig/network
+
+
