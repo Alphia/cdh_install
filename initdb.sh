dc63faa5f74ac8e3844cd2587fe39d70857a154e
root <root@host0.mycluster.com>
2021-01-12 16:51:21 +0800

    typo for the sql file location

--- initdb.sh
+++ initdb.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-su -l postgres -c "psql -f /var/lib/pgsql/config_db.sql"
+su -l postgres -c "psql -f ./setup_db_name_and_owner.sql"
 /opt/cloudera/cm/schema/scm_prepare_database.sh postgresql scm scm 1qaz2ws
 
