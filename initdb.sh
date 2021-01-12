85c7e54cebe3855164fe6a4eb9e0c54571c13b91
root <root@host0.mycluster.com>
2021-01-12 17:17:14 +0800

    fix file owner issue

--- initdb.sh
+++ initdb.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
-su -l postgres -c "psql -f ./setup_db_name_and_owner.sql"
+cp setup_db_name_and_owner.sql /var/lib/pgsql/setup_db_name_and_owner.sql
+chmod 744 /var/lib/pgsql/setup_db_name_and_owner.sql
+su -l postgres -c "psql -f setup_db_name_and_owner.sql"
 /opt/cloudera/cm/schema/scm_prepare_database.sh postgresql scm scm 1qaz2ws
 
